0f3beaa9b402eb90fa32ef264aef475750fbe903
matt oballe <oballematt@gmail.com>
2022-01-11 21:43:01 -0600

    adding input group for input select options.

--- controllers/marsRover.js
+++ controllers/marsRover.js
@@ -17,7 +17,7 @@ module.exports = {
   getDefaultImages: async (req, res) => {
     let today = new Date();
     let dd = String(today.getDate()).padStart(2, '0');
-    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let mm = String(today.getMonth() + 1).padStart(2, '0');
     let yyyy = today.getFullYear();
   
     today = yyyy + '-' + mm + '-' + dd 
